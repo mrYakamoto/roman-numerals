ec1a7aec5781169cdf2912e5839ff49da5fad3c1
Torey Hickman <torey@toreyhickman.com>
2015-05-18 12:56:04 -0500

    Set up spec file for old and modern sytle roman numerals

--- source/spec/roman_numerals_spec.rb
+++ source/spec/roman_numerals_spec.rb
@@ -1,11 +1,16 @@
 require_relative '../roman_numerals'
 
 describe 'converting an Arabic number to a Roman numeral' do
-  it 'should convert 1 to I' do
-    expect(convert_to_roman(1)).to eq("I")
+  describe 'old Roman numerals' do
+    it 'should convert 1 to I' do
+      expect(convert_to_roman(1)).to eq("I")
+    end
+
+    it 'should convert 3 to III' do
+      expect(convert_to_roman(3)).to eq("III")
+    end
   end
 
-  it 'should convert 3 to III' do
-    expect(convert_to_roman(3)).to eq("III")
+  describe 'modern Roman numerals' do
   end
 end
