c6131ef86a96dd2f9241ca981bc046507fa7a096
Torey Hickman <torey@toreyhickman.com>
2015-05-18 12:15:20 -0500

    Change method name

--- source/roman_numerals.rb
+++ source/roman_numerals.rb
@@ -1,6 +1,6 @@
 #Test your code by running the spec in the terminal:
 #rspec roman_numerals_spec.rb
 
-def to_roman(num)
+def convert_to_roman(num)
   #Your code goes here
 end
--- source/spec/roman_numerals_spec.rb
+++ source/spec/roman_numerals_spec.rb
@@ -2,10 +2,10 @@ require_relative '../roman_numerals'
 
 describe 'converting an Arabic number to a Roman numeral' do
   it 'should convert 1 to I' do
-    expect(to_roman(1)).to eq("I")
+    expect(convert_to_roman(1)).to eq("I")
   end
 
   it 'should convert 3 to III' do
-    expect(to_roman(3)).to eq("III")
+    expect(convert_to_roman(3)).to eq("III")
   end
 end
