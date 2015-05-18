0ad80d33f8f5c93b30ecdaef77570de458e9bed6
Torey Hickman <torey@toreyhickman.com>
2015-05-18 13:37:46 -0500

    Add comments to spec file alerting students where to add which tests

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -9,8 +9,15 @@ describe 'converting an Arabic number to a Roman numeral' do
     it 'should convert 4 to IIII' do
       expect(convert_to_roman(4)).to eq("IIII")
     end
+
+    # Release 1 ...
+    # add tests for old roman numerals here
+
   end
 
   describe 'modern Roman numerals' do
+    # Release 2 ...
+    # add tests for modern roman numerals here
+
   end
 end
