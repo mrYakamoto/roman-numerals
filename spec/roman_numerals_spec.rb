133fa03750cfea3b6c666c2cd8c1ca6f0f173f96
Torey Hickman <torey@toreyhickman.com>
2015-05-18 15:03:54 -0500

    spec syntax

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -3,11 +3,11 @@ require_relative '../roman_numerals'
 describe 'converting an Arabic number to a Roman numeral' do
   describe 'old Roman numerals' do
     it 'converts 1 to I' do
-      expect(convert_to_roman(1)).to eq("I")
+      expect(convert_to_roman(1)).to eq "I"
     end
 
     it 'converts 4 to IIII' do
-      expect(convert_to_roman(4)).to eq("IIII")
+      expect(convert_to_roman(4)).to eq "IIII"
     end
 
     # Release 1 ...
