d8d7272830e43c065d92667582cecc6fdffda86b
Torey Hickman <torey@toreyhickman.com>
2015-05-18 12:14:59 -0500

    Update example group description

--- source/spec/roman_numerals_spec.rb
+++ source/spec/roman_numerals_spec.rb
@@ -1,9 +1,6 @@
-require_relative 'roman_numerals'
+require_relative '../roman_numerals'
 
-#Here's a collection of tests for your to_roman method
-#You'll need to add your own tests too!
-
-describe 'to_roman' do
+describe 'converting an Arabic number to a Roman numeral' do
   it 'should convert 1 to I' do
     expect(to_roman(1)).to eq("I")
   end
