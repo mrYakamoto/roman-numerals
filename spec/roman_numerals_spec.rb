bc8ce6b947fa810bbcce9515e0c9754416905048
Torey Hickman <torey@toreyhickman.com>
2015-05-18 15:02:28 -0500

    Remove should language from tests

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -2,11 +2,11 @@ require_relative '../roman_numerals'
 
 describe 'converting an Arabic number to a Roman numeral' do
   describe 'old Roman numerals' do
-    it 'should convert 1 to I' do
+    it 'converts 1 to I' do
       expect(convert_to_roman(1)).to eq("I")
     end
 
-    it 'should convert 4 to IIII' do
+    it 'converts 4 to IIII' do
       expect(convert_to_roman(4)).to eq("IIII")
     end
 
